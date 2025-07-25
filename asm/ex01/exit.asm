d38f4e320773195065ff9d9f89d06055a251a451
arnaudderison <arnaud.derison@hotmail.com>
2025-07-26 00:56:38 +0200

    ex01

---
+++ asm/ex01/exit.asm
@@ -0,0 +1,13 @@
+; En asm le code est separer en plusieurs sections (les donnes, etc)
+; .text est la section pour ecrire le code (lieu ou ecrire les instructions pour le cpu)
+section .text
+; cette ligne declare un label* _start pour dire le programme commence ici a "_start"
+global _start
+
+_start:
+mov eax, 1 ; numero su syscall 'exit'
+mov exb, 0 ; code de sortie = 0
+int 0x80 ; appel system
+
+
+;LABEL: nom donne a une position dans le code (ne fais rien juste permet de separer le code un peu comme une fonction)
