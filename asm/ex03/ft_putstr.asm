fc17311153390ba872e21dc474d586b0121b7891
arnaudderison <arnaud.derison@hotmail.com>
2025-07-26 20:29:44 +0200

    putstr

---
+++ asm/ex03/ft_putstr.asm
@@ -0,0 +1,24 @@
+section .text
+    global ft_putstr
+    extern ft_strlen
+    
+ft_putstr:
+    push ebp
+    mov ebp, esp
+
+    mov esi, [ebp + 8]    ; ESI ← adresse de la chaîne (1er argument)
+    push esi              ; sauvegarde ESI pour ft_strlen
+    call ft_strlen        ; EAX ← longueur
+    pop esi               ; restaure ESI
+
+    mov edx, eax          ; EDX ← longueur
+    mov ecx, esi          ; ECX ← adresse de la chaîne
+    mov ebx, 1            ; EBX ← descripteur (1 = stdout)
+    mov eax, 4            ; EAX ← syscall write
+    int 0x80              ; appel système
+
+    mov esp, ebp
+    pop ebp
+    ret
+
+section .note.GNU-stack noalloc noexec nowrite progbits
---
+++ asm/ex03/test.c
@@ -0,0 +1,7 @@
+extern void ft_putstr(const char *s);
+
+int main()
+{
+    ft_putstr("Coucou je suis un super text");
+    return 0;
+}
